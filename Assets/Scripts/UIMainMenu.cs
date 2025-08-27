ff9b4a82061ae205fed1b0fcb1433d72d4b9711b
bABCDd <rlatkdrbs6845@gmail.com>
2025-08-27 22:10:32 +0900

    [feat] Step1 UI생성하기

---
+++ Assets/Scripts/UIMainMenu.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class UIMainMenu : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI IDText;
+    [SerializeField] private TextMeshProUGUI CurrentLv;
+    [SerializeField] private TextMeshProUGUI CurrentExp;
+    [SerializeField] private TextMeshProUGUI CurrentGold;
+    public Image ExpImgFilled;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
